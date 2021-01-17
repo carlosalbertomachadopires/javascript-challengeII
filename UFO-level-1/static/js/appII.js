86dc4c69a3c81c771d1fc1e54f0fd97ac3e14c88
CPDEZ08 <carlosalbertomachadopires@gmail.comm>
2021-01-16 21:06:32 -0600

    update final HW files

---
+++ UFO-level-1/static/js/appII.js
@@ -0,0 +1,65 @@
+function showData(data) {
+    
+    let table = d3.select("#data-table")
+
+    table.selectAll("tr").remove()
+    
+    let headerRow = table.append("tr")
+
+    headerRow.append("th").text("Date")
+    headerRow.append("th").text("City")
+    headerRow.append("th").text("State")
+    headerRow.append("th").text("Country")
+    headerRow.append("th").text("Shape")
+    headerRow.append("th").text("Duration")
+    headerRow.append("th").text("Comments")
+
+    let tableBody = table.append("tbody")
+
+    data.forEach((d) => {
+        let row = tableBody.append("tr")
+        row.append("td").text(d.datetime)
+        row.append("td").text(d.city)
+        row.append("td").text(d.state)
+        row.append("td").text(d.country)
+        row.append("td").text(d.shape)
+        row.append("td").text(d.durationMinutes)
+        row.append("td").text(d.comments)
+
+    })
+    
+}
+
+// function handleClick() {
+//     let datetime = d3.select("#date-filter-input").property("value")
+//     let filteredData = data.filter((d) => d.datetime === datetime)
+//     showData(filteredData)
+
+// }
+
+// d3.select("#table-button").on("click", handleClick)
+function handleClick() {
+    let city = d3.select("#city-filter-input").property("value")
+    let filteredData = data.filter((d) => d.city === city)
+    showData(filteredData)
+
+}
+d3.select("#city-button").on("click", handleClick)
+// function handleClick() {
+//     let state = d3.select("#state-filter-input").property("value")
+//     let filteredData = data.filter((d) => d.state === state)
+//     showData(filteredData)
+// }
+// d3.select("#state-button").on("click", handleClick) 
+
+
+
+ 
+
+
+
+// d3.select("#table-button").on("click", handleClick)
+// d3.select("#filter-btn").on("click", showData)
+
+
+
